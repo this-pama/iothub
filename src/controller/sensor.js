f79812ad025f0aeb780db70a24eba730e217d842
Dapo Aderemi <adedapopaul@yahoo.com>
2020-02-28 11:26:05 +0100

    log webhook

--- src/controller/sensor.js
+++ src/controller/sensor.js
@@ -100,7 +100,7 @@ api.post('/add/:userId',authenticate, (req, res) => {
 
 
   // '/v1/sensor/application/webhook' - POST - update a a sensor  { id is user ID}
-  api.post('application/webhook', (req, res) => {
+  api.post('/application/webhook', (req, res) => {
     console.log(req.body)
     var publicKey = req.headers["Authorization"];
     var privateKey = req.headers["privateKey"];
